bfc09e363411c6ce45653b649307ba733f82d863
Dron84 <dron84@gmail.com>
2024-10-10 18:54:44 +0300

    add route Facades in to use section

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FurnitureController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('furniture', [FurnitureController::class, 'index']);
 Route::get('furniture/{id}', [FurnitureController::class, 'show']);
